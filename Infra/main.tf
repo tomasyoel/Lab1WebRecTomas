a795c94fd0c94d01451b46e6acd2f157cae00a15
tomasyoel <tomcondoriv@upt.pe>
2025-01-14 17:03:11 -0500

    ver

--- Infra/main.tf
+++ Infra/main.tf
@@ -8,33 +8,28 @@ terraform {
   }
   required_version = ">= 0.14.9"
 }
-
 provider "azurerm" {
   features {}
   subscription_id = "286afa39-6818-4e97-bdf4-c2c00c2e75e4"
 }
 
-# Variable for existing resource group
-variable "resource_group_name" {
-  default = "upt-arg-145"
-}
-
-# Generate a random integer to create a globally unique name for dependent resources
+# Generate a random integer to create a globally unique name
 resource "random_integer" "ri" {
   min = 100
   max = 999
 }
 
-# Reference the existing resource group
-data "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "upt-arg-${random_integer.ri.result}"
+  location = "eastus"
 }
 
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
   name                = "upt-asp-${random_integer.ri.result}"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
   sku_name            = "F1"
 }
@@ -42,13 +37,14 @@ resource "azurerm_service_plan" "appserviceplan" {
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
   name                  = "upt-awa-${random_integer.ri.result}"
-  location              = data.azurerm_resource_group.rg.location
-  resource_group_name   = data.azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
   service_plan_id       = azurerm_service_plan.appserviceplan.id
   depends_on            = [azurerm_service_plan.appserviceplan]
+  //https_only            = true
   site_config {
     minimum_tls_version = "1.2"
-    always_on           = false
+    always_on = false
     application_stack {
       dotnet_version = "8.0"
     }
